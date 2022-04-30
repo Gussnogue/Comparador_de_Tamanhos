caa18301ea695b17120c73b7c66c59026e1d3454
Gussnogue <gustavosilvanogueira@hotmail.com>
2022-04-30 17:30:37 -0300

    Comparador de tamanho utilizando Python

---
+++ Comparador de tamanho.py
@@ -0,0 +1,16 @@
+sizes = [7340, 2117, 22300, 31700, 1679, 1014, 23000, 9910, 685]
+
+largest = max(sizes)
+
+print("Largest lake in sq mi:")
+print(largest)
+
+smallest = min(sizes)
+
+print("10th largest lake in sq mi:")
+print(smallest)
+
+difference = largest - smallest
+
+print("Difference bwtween lakes in sq mi:")
+print(difference)
